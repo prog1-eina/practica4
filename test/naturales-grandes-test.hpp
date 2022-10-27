cae61b6c401205bb6382e1a9c8794ee521d79fa9
Miguel Ángel Latre Abadía <latre@unizar.es>
2022-10-27 19:30:12 +0200

    Update naturales-grandes-test.hpp

--- test/naturales-grandes-test.hpp
+++ test/naturales-grandes-test.hpp
@@ -1,13 +1,13 @@
-﻿/******************************************************************************\
+﻿/*********************************************************************************
  * Programación 1. Práctica 4
  * Autor: Miguel Ángel Latre Abadía
- * Ultima revisión: 10 de noviembre de 2018
- * Resumen: Fichero de interfaz «naturales-grandes-test.h» de un módulo
+ * Ultima revisión: 21 de octubre de 2022
+ * Resumen: Fichero de interfaz «naturales-grandes-test.hpp» de un módulo
  *          denominado «naturales-grandes-test» para hacer de pruebas de las
- *          funciones definidas en el módulo «naturales-grandes» de la
- *          4ª práctica.
+ *          funciones definidas en el módulo «naturales-grandes» de la 4.ª
+ *          práctica.
  * Codificación de caracteres original de este fichero: UTF-8 con BOM
-\******************************************************************************/
+\********************************************************************************/
 
 #include <string>
 using namespace std;
@@ -15,10 +15,10 @@ using namespace std;
 /*
  * Pre:  «valorEsperado» es una cadena de caracteres que contiene la 
  *       representación decimal de un número natural (sin signo).
- * Post: Ha escrito un mensaje en pantalla informando acerca de si la función
- *       «convertir», invocada con el argumento «n», ha calculado el valor
- *       correcto «valorEsperado». En caso negativo, ha informado
- *       también acerca del valor incorrecto que calculó la función.
+ * Post: Escribe un mensaje en pantalla informando acerca de si la función
+ *       «convertir», invocada con el argumento «n», calcula el valor
+ *       correcto «valorEsperado». En caso negativo, informa
+ *       también acerca del valor incorrecto que calcula la función.
  */
 void probarConvertir(const unsigned n, const string valorEsperado);
 
@@ -26,10 +26,10 @@ void probarConvertir(const unsigned n, const string valorEsperado);
  * Pre:  «a», «b» y «valorEsperado» son cadenas de caracteres que contienen la 
  *       representación decimal de números naturales (sin signo).
  *       «valorEsperado», en particular, representa la suma de «a» y «b».
- * Post: Ha escrito un mensaje en pantalla informando acerca de si la función
- *       «sumar», invocada con los argumentos «a» y «b», ha calculado el valor
- *       correcto «valorEsperado». En caso negativo, ha informado
- *       también acerca del valor incorrecto que calculó la función.
+ * Post: Escribe un mensaje en pantalla informando acerca de si la función
+ *       «sumar», invocada con los argumentos «a» y «b», calcula el valor
+ *       correcto «valorEsperado». En caso negativo, informa
+ *       también acerca del valor incorrecto que calcula la función.
  */
 void probarSumar(const string a, const string b, const string valorEsperado);
 
@@ -38,10 +38,10 @@ void probarSumar(const string a, const string b, const string valorEsperado);
  * Pre:  «numero» es una cadena de caracteres que contiene la representación
  *       decimal de un número natural (sin signo). «valorEsperado» es el valor 
  *       numérico representado por la secuencia «numero».
- * Post: Ha escrito un mensaje en pantalla informando acerca de si la función
- *       «valor», invocada con el argumento «numero», ha devuelto el valor
- *       correcto «valorEsperado». En caso negativo, ha informado también acerca
- *       del valor incorrecto que devolvió la función.
+ * Post: Escribe un mensaje en pantalla informando acerca de si la función
+ *       «valor», invocada con el argumento «numero», devuelve el valor
+ *       correcto «valorEsperado». En caso negativo, informa también acerca
+ *       del valor incorrecto que devuelve la función.
  */
 void probarValor(const string numero, const unsigned valorEsperado);
 
@@ -50,10 +50,10 @@ void probarValor(const string numero, const unsigned valorEsperado);
  * Pre:  «numero» es una cadena de caracteres que contiene la representación
  *       decimal de un número natural (sin signo). «valorEsperado» es la
  *       representación de «numero», pero con sus cifras en orden inverso.
- * Post: Ha escrito un mensaje en pantalla informando acerca de si la función
- *       «calcularImagen», invocada con el argumento «numero», ha calculado el
- *       valor correcto «valorEsperado». En caso negativo, ha informado también
- *       acerca del valor incorrecto que devolvió la función.
+ * Post: Escribe un mensaje en pantalla informando acerca de si la función
+ *       «calcularImagen», invocada con el argumento «numero», calcula el
+ *       valor correcto «valorEsperado». En caso negativo, informa también
+ *       acerca del valor incorrecto que devuelve la función.
  */
 void probarCalcularImagen(const string numero, const string valorEsperado);
 
@@ -62,10 +62,10 @@ void probarCalcularImagen(const string numero, const string valorEsperado);
  * Pre:  «numero» es una cadena de caracteres que contiene la representación
  *       decimal de un número natural (sin signo). «valorEsperado» indica si 
  *       «numero» representa un número capicúa («true») o no («false»).
- * Post: Ha escrito un mensaje en pantalla informando acerca de si la función
- *       «esCapicua», invocada con el argumento «numero», ha devuelto el valor
- *       correcto «valorEsperado». En caso negativo, ha informado también acerca
- *       del valor incorrecto que devolvió la función.
+ * Post: Escribe un mensaje en pantalla informando acerca de si la función
+ *       «esCapicua», invocada con el argumento «numero», devuelve el valor
+ *       correcto «valorEsperado». En caso negativo, informa también acerca
+ *       del valor incorrecto que devuelve la función.
  */
 void probarEsCapicua(const string numero, const bool valorEsperado);
 
@@ -73,10 +73,10 @@ void probarEsCapicua(const string numero, const bool valorEsperado);
 /*
  * Pre:  «numero» es una cadena de caracteres que contiene la representación
  *       decimal de un número natural (sin signo).
- * Post: Ha escrito un mensaje en pantalla informando acerca de si la función
- *       «copiar», invocada con el argumento «numero», ha realizado una copia
- *       correcta de su argumento de entrada. En caso negativo, ha informado 
- *       también acerca del valor incorrecto que devolvió la función.
+ * Post: Escribe un mensaje en pantalla informando acerca de si la función
+ *       «copiar», invocada con el argumento «numero», realiza una copia
+ *       correcta de su argumento de entrada. En caso negativo, informa 
+ *       también acerca del valor incorrecto que devuelve la función.
  */
 void probarCopiar(const string numero);
 
@@ -94,9 +94,9 @@ void probarSonIguales(const string a, const string b, const bool valorEsperado);
  * Pre:  «numero» es una cadena de caracteres que contiene la representación
  *       decimal de un número natural (sin signo). «valorEsperado» es el número
  *       de cifras de «numero».
- * Post: Ha escrito un mensaje en pantalla informando acerca de si la función
- *       «numCifras», invocada con el argumento «numero», ha calculado el
- *       valor correcto «valorEsperado». En caso negativo, ha informado también
- *       acerca del valor incorrecto que devolvió la función.
+ * Post: Escribe un mensaje en pantalla informando acerca de si la función
+ *       «numCifras», invocada con el argumento «numero», calcula el
+ *       valor correcto «valorEsperado». En caso negativo, informa también
+ *       acerca del valor incorrecto que devuelve la función.
  */
 void probarNumCifras(const string numero, const unsigned valorEsperado);
