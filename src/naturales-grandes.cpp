d76787fc9a1f650d4a6c3b9ee3030b3e22c372ea
Miguel Ángel Latre Abadía <latre@unizar.es>
2025-11-13 13:55:40 +0100

    Refactor comments and formatting in naturales-grandes

--- src/naturales-grandes.cpp
+++ src/naturales-grandes.cpp
@@ -14,12 +14,10 @@
  *       componente original[0] representa las unidades de «original», la
  *       componente original[1] las decenas de «original» y así, sucesivamente.
  *       Las componentes de índices superiores al correspondiente al dígito más
- *       significativo almacenan el valor 0. «original» y «copia» tienen 
- *       «NUM_DIGITOS» componentes.
+ *       significativo de cada uno almacenan el valor 0.
+ *       «original» y «copia» tienen «NUM_DIGITOS» componentes.
  * Post: Tras ejecutar este procedimiento, «copia» almacena una representación
  *       idéntica a la del número natural correspondiente a la de «original».
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
  */
 void copiar(const unsigned original[], unsigned copia[]) {
     // Por completar
@@ -27,18 +25,15 @@ void copiar(const unsigned original[], unsigned copia[]) {
 
 
 /*
- * Pre:  «a» y «b» almacenan las representaciones de sendos números naturales. 
- *       Las componentes a[0] y b[0] representan las unidades del natural «a» y 
- *       «b», respectivamente, las componentes a[1] y b[1], las decenas y así 
- *       sucesivamente. Las componentes de índices
- *       superiores a los correspondientes a los dígitos más significativos
- *       almacenan el valor 0. Las componentes de 
- *       índices superiores almacenan el valor 0. «a» y «b» tienen «NUM_DIGITOS»
- *       componentes.
- * Post: Devuelve el valor booleano true si «a» y «b» representan el mismo
- *       número natural y false en caso contrario.
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
+ * Pre:  «a» y «b» almacenan las representaciones de sendos números naturales.
+ *       Las componentes a[0] y b[0] representan las unidades del natural «a» y
+ *       «b», respectivamente, las componentes a[1] y b[1], las decenas y así
+ *       sucesivamente. Las componentes de índices superiores a los
+ *       correspondientes a los dígitos más significativos almacenan el
+ *       valor 0. Las componentes de índices superiores almacenan el valor 0.
+ *       «a» y «b» tienen «NUM_DIGITOS» componentes.
+ * Post: Devuelve el valor booleano «true» si «a» y «b» representan el mismo
+ *       número natural y «false» en caso contrario.
  */
 bool sonIguales(const unsigned a[], const unsigned b[]) {
     // Por completar
@@ -46,29 +41,29 @@ bool sonIguales(const unsigned a[], const unsigned b[]) {
 
 
 /*
- * Pre:  «natural» almacena la representación de un número natural. La 
+ * Pre:  «natural» almacena la representación de un número natural. La
  *       componente natural[0] representa las unidades de «natural», la
- *       componente natural[1] las decenas de «natural» y así, sucesivamente. 
+ *       componente natural[1] las decenas de «natural» y así, sucesivamente.
  *       Las componentes de índices superiores al correspondiente al dígito más
- *       significativo almacenan el valor 0. «natural» tiene «NUM_DIGITOS»
- *       componentes.
+ *       significativo almacenan el valor 0.
+ *       «natural» tiene «NUM_DIGITOS» componentes.
  * Post: Devuelve el número de cifras de «natural» cuando se escribe en base 10
  *       sin ceros a la izquierda.
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
  */
 unsigned numCifras(const unsigned natural[]) {
     // Por completar
 }
 
+
 /*
- * Pre:  «natural» almacena la representación de un número natural grande. La componente
- *       natural[0] representa las unidades de «natural», la componente natural[1] las decenas
- *       de «natural» y así, sucesivamente. Las componentes de índices superiores al
- *       correspondiente al dígito más significativo almacenan el valor 0.
+ * Pre:  «natural» almacena la representación de un número natural grande. La
+ *       componente natural[0] representa las unidades de «natural», la
+ *       componente natural[1] las decenas de «natural» y así, sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
  *       «natural» tiene «NUM_DIGITOS» componentes.
- * Post: Tras ejecutar este procedimiento, se ha escrito en la pantalla el número natural
- *       representado por «natural» (sin ceros a la izquierda).
+ * Post: Tras ejecutar este procedimiento, se ha escrito en la pantalla el
+ *       número natural representado por «natural» (sin ceros a la izquierda).
  */
 void escribirEnPantalla(const unsigned natural[]) {
     // Por completar
@@ -77,14 +72,12 @@ void escribirEnPantalla(const unsigned natural[]) {
 
 /*
  * Pre:  «naturalGrande» tiene «NUM_DIGITOS» componentes.
- * Post: Tras ejecutar este procedimiento, «naturalGrande» contiene la secuencia
- *       de dígitos equivalente a «numero». 
- *       La componente naturalGrande[0] almacena las unidades de «numero», la
- *       componente naturalGrande[1], las decenas de «numero» y así 
- *       sucesivamente. Las componentes de índices superiores al correspondiente
- *       al dígito más significativo almacenan el valor 0.
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
+ * Post: Tras ejecutar este procedimiento, «naturalGrande» contiene la
+ *       secuencia de dígitos equivalente a «numero». La componente
+ *       naturalGrande[0] almacena las unidades de «numero», la componente
+ *       naturalGrande[1], las decenas de «numero» y así sucesivamente. Las
+ *       componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
  */
 void convertir(const unsigned numero, unsigned naturalGrande[]) {
     // Por completar
@@ -92,16 +85,15 @@ void convertir(const unsigned numero, unsigned naturalGrande[]) {
 
 
 /*
- * Pre:  «naturalGrande» almacena los dígitos de un número natural lo 
+ * Pre:  «naturalGrande» almacena los dígitos de un número natural lo
  *       suficientemente pequeño como para ser representado por un dato de tipo
  *       «unsigned». En la componente naturalGrande[0] se almacena el dígito
- *       que representa las unidades de ese número, en la componente 
+ *       que representa las unidades de ese número, en la componente
  *       naturalGrande[1], las decenas y así sucesivamente. Las componentes de
  *       índices superiores al correspondiente al dígito más significativo
- *       almacenan el valor 0. «naturalGrande» tiene «NUM_DIGITOS» componentes.
+ *       almacenan el valor 0. «naturalGrande» tiene «NUM_DIGITOS»
+ *       componentes.
  * Post: Devuelve el valor numérico del natural almacenado en «naturalGrande».
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
  */
 unsigned valor(const unsigned naturalGrande[]) {
     // Por completar
@@ -110,40 +102,42 @@ unsigned valor(const unsigned naturalGrande[]) {
 
 /*
  * Pre:  «a» y «b» almacenan las representaciones de sendos números naturales. 
- *       Las componentes a[0] y b[0] representan las unidades del natural «a» y «b»,
- *       respectivamente, las componentes a[1] y b[1], las decenas y así sucesivamente. Las
- *       componentes de índices superiores a los correspondientes a los dígitos más
- *       significativos almacenan el valor 0.
+ *       Las componentes a[0] y b[0] representan las unidades del natural «a» y
+ *       «b», respectivamente, las componentes a[1] y b[1], las decenas y así
+ *       sucesivamente. Las componentes de índices superiores a los
+ *       correspondientes a los dígitos más significativos almacenan el
+ *       valor 0.
  *       Los vectores «a», «b» y «suma» tienen «NUM_DIGITOS» componentes.
- * Post: Si la suma de los naturales representados por «a» y «b» tiene «NUM_DIGITOS» o menos,
- *       tras ejecutarse este procedimiento, «suma» almacena la representación del número
- *       natural equivalente a la suma de los naturales representados por «a» y «b» y el valor
- *       de «desbordamiento» es «false». En el caso contrario, el valor de «suma» no está
- *       definido y el valor de «desbordamiento» es «true». 
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
+ * Post: Si la suma de los naturales representados por «a» y «b» tiene
+ *       «NUM_DIGITOS» o menos, tras ejecutarse este procedimiento, «suma»
+ *       almacena la representación del número natural equivalente a la suma de
+ *       los naturales representados por «a» y «b» y el valor de
+ *       «desbordamiento» es «false». En el caso contrario, el valor de «suma»
+ *       no está definido y el valor de «desbordamiento» es «true». 
  */
-void sumar(const unsigned a[], const unsigned b[], unsigned suma[], bool &desbordamiento) {
+void sumar(const unsigned a[], const unsigned b[],
+           unsigned suma[], bool &desbordamiento) {
     // Por completar
 }
 
 
 /*
- * Pre:  «a» almacena la representación de un número natural. La componente a[0] representa las
- *       unidades del natural «a», la componente a[1], las decenas y así sucesivamente. Las
- *       componentes de índices superiores a los correspondientes a los dígitos más
- *       significativos almacenan el valor 0.
+ * Pre:  «a» almacena la representación de un número natural. La componente
+ *       a[0] representa las unidades del natural «a», la componente a[1], las
+ *       decenas y así sucesivamente. Las componentes de índices superiores a
+ *       los correspondientes a los dígitos más significativos almacenan el
+ *       valor 0.
  *       Los vectores «a» y «producto» tienen «NUM_DIGITOS» componentes.
- * Post: Si el producto de los naturales representados por «a» y el valor de «b» tiene
- *       «NUM_DIGITOS» o menos, tras ejecutarse este procedimiento, «producto» almacena la
- *       representación del número natural equivalente al producto de los naturales
- *       representados por «a» y el valor de «b» y el valor de «desbordamiento» es «false».
- *       En el caso contrario, el valor de «producto» no está definido y el valor de
- *       «desbordamiento» es «true».
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
+ * Post: Si el producto de los naturales representados por «a» y el valor de
+ *       «b» tiene «NUM_DIGITOS» o menos, tras ejecutarse este procedimiento,
+ *       «producto» almacena la representación del número natural equivalente
+ *       al producto de los naturales representados por «a» y el valor de «b» y
+ *       el valor de «desbordamiento» es «false». En el caso contrario, el
+ *       valor de «producto» no está definido y el valor de «desbordamiento»
+ *       es «true».
  */
-void multiplicar(const unsigned a[], const unsigned b, unsigned producto[], bool &desbordamiento) {
+void multiplicar(const unsigned a[], const unsigned b,
+                 unsigned producto[], bool &desbordamiento) {
     // Por completar
 }
 
@@ -151,14 +145,12 @@ void multiplicar(const unsigned a[], const unsigned b, unsigned producto[], bool
 /*
  * Pre:  «natural» almacena la representación de un número natural. La
  *       componente natural[0] representa las unidades de «natural», la
- *       componente natural[1] las decenas de «natural» y así sucesivamente. Las
- *       componentes de índices superiores al correspondiente al dígito más
- *       significativo almacenan el valor 0. «natural» e «imagen» tienen
- *       «NUM_DIGITOS» componentes.
+ *       componente natural[1] las decenas de «natural» y así sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
+ *       «natural» e «imagen» tienen «NUM_DIGITOS» componentes.
  * Post: Tras ejecutar este procedimiento, «imagen» almacena la representación
  *       del número natural correspondiente a la imagen especular de «natural».
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
  */
 void calcularImagen(const unsigned natural[], unsigned imagen[]) {
     // Por completar
@@ -168,13 +160,11 @@ void calcularImagen(const unsigned natural[], unsigned imagen[]) {
 /*
  * Pre:  «natural» almacena la representación de un número natural. La
  *       componente natural[0] representa las unidades de «natural», la
- *       componente natural[1] las decenas de «natural» y así sucesivamente. Las
- *       componentes de índices superiores al correspondiente al dígito más
- *       significativo almacenan el valor 0. «natural» tiene «NUM_DIGITOS»
- *       componentes.
+ *       componente natural[1] las decenas de «natural» y así sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
+ *       «natural» tiene «NUM_DIGITOS» componentes.
  * Post: Devuelve «true» si y solo si «natural» es un número capicúa.
- * NOTA: Hay explicaciones adicionales sobre este procedimiento en el enunciado
- *       de la práctica.
  */
 bool esCapicua(const unsigned natural[]) {
     // Por completar
--- src/naturales-grandes.hpp
+++ src/naturales-grandes.hpp
@@ -1,128 +1,143 @@
-﻿/*********************************************************************************************\
+﻿/******************************************************************************
  * Programación 1. Práctica 4
  * Autores: Javier Martínez y Miguel Ángel Latre
- * Última revisión: 23 de octubre de 2024
+ * Última revisión: 13 de noviembre de 2025
  * Resumen: Fichero de interfaz «naturales-grandes.hpp» de un módulo denominado
- *          «naturales-grandes» para trabajar con números naturales de gran magnitud en la 4ª
- *          práctica.
-\*********************************************************************************************/
+ *          «naturales-grandes» para trabajar con números naturales de gran
+ *          magnitud en la 4ª práctica.
+ *****************************************************************************/
 
 #pragma once
 
 const unsigned NUM_DIGITOS = 1000;
 
 /*
- * Pre:  «original» almacena la representación de un número natural. La componente original[0]
- *       representa las unidades de «original», la componente original[1] las decenas de
- *       «original» y así, sucesivamente. Las componentes de índices superiores al
- *       correspondiente al dígito más significativo almacenan el valor 0. «original» y
- *        «copia» tienen «NUM_DIGITOS» componentes.
- * Post: Tras ejecutar este procedimiento, «copia» almacena una representación idéntica a la
- *       del número natural correspondiente a la de «original».
+ * Pre:  «original» almacena la representación de un número natural. La
+ *       componente original[0] representa las unidades de «original», la
+ *       componente original[1] las decenas de «original» y así, sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo de cada uno almacenan el valor 0.
+ *       «original» y «copia» tienen «NUM_DIGITOS» componentes.
+ * Post: Tras ejecutar este procedimiento, «copia» almacena una representación
+ *       idéntica a la del número natural correspondiente a la de «original».
  */
 void copiar(const unsigned original[], unsigned copia[]);
 
 /*
- * Pre:  «a» y «b» almacenan las representaciones de sendos números naturales. Las componentes
- *       a[0] y b[0] representan las unidades del natural «a» y «b», respectivamente, las
- *       componentes a[1] y b[1], las decenas y así sucesivamente. Las componentes de índices
- *       superiores a los correspondientes a los dígitos más significativos almacenan el
- *       valor 0. Las componentes de índices superiores almacenan el valor 0. «a» y «b» tienen
- *       «NUM_DIGITOS» componentes.
- * Post: Devuelve el valor booleano «true» si «a» y «b» representan el mismo número natural y
- *       «false» en caso contrario.
+ * Pre:  «a» y «b» almacenan las representaciones de sendos números naturales.
+ *       Las componentes a[0] y b[0] representan las unidades del natural «a» y
+ *       «b», respectivamente, las componentes a[1] y b[1], las decenas y así
+ *       sucesivamente. Las componentes de índices superiores a los
+ *       correspondientes a los dígitos más significativos almacenan el
+ *       valor 0. Las componentes de índices superiores almacenan el valor 0.
+ *       «a» y «b» tienen «NUM_DIGITOS» componentes.
+ * Post: Devuelve el valor booleano «true» si «a» y «b» representan el mismo
+ *       número natural y «false» en caso contrario.
  */
 bool sonIguales(const unsigned a[], const unsigned b[]);
 
 /*
- * Pre:  «natural» almacena la representación de un número natural. La componente natural[0]
- *       representa las unidades de «natural», la componente natural[1] las decenas de
- *       «natural» y así, sucesivamente. Las componentes de índices superiores al
- *       correspondiente al dígito más significativo almacenan el valor 0. «natural» tiene
- *        «NUM_DIGITOS» componentes.
- * Post: Devuelve el número de cifras de «natural» cuando se escribe en base 10 sin ceros a la
- *       izquierda.
+ * Pre:  «natural» almacena la representación de un número natural. La
+ *       componente natural[0] representa las unidades de «natural», la
+ *       componente natural[1] las decenas de «natural» y así, sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
+ *       «natural» tiene «NUM_DIGITOS» componentes.
+ * Post: Devuelve el número de cifras de «natural» cuando se escribe en base 10
+ *       sin ceros a la izquierda.
  */
 unsigned numCifras(const unsigned natural[]);
 
 /*
- * Pre:  «natural» almacena la representación de un número natural grande. La componente
- *       natural[0] representa las unidades de «natural», la componente natural[1] las decenas
- *       de «natural» y así, sucesivamente. Las componentes de índices superiores al
- *       correspondiente al dígito más significativo almacenan el valor 0.
+ * Pre:  «natural» almacena la representación de un número natural grande. La
+ *       componente natural[0] representa las unidades de «natural», la
+ *       componente natural[1] las decenas de «natural» y así, sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
  *       «natural» tiene «NUM_DIGITOS» componentes.
- * Post: Tras ejecutar este procedimiento, se ha escrito en la pantalla el número natural
- *       representado por «natural» (sin ceros a la izquierda).
+ * Post: Tras ejecutar este procedimiento, se ha escrito en la pantalla el
+ *       número natural representado por «natural» (sin ceros a la izquierda).
  */
 void escribirEnPantalla(const unsigned natural[]);
 
 /*
  * Pre:  «naturalGrande» tiene «NUM_DIGITOS» componentes.
- * Post: Tras ejecutar este procedimiento, «naturalGrande» contiene la secuencia de dígitos
- *       equivalente a «numero». La componente naturalGrande[0] almacena las unidades de
- *       «numero», la componente naturalGrande[1], las decenas de «numero» y así 
- *       sucesivamente. Las componentes de índices superiores al correspondiente al dígito más
+ * Post: Tras ejecutar este procedimiento, «naturalGrande» contiene la
+ *       secuencia de dígitos equivalente a «numero». La componente
+ *       naturalGrande[0] almacena las unidades de «numero», la componente
+ *       naturalGrande[1], las decenas de «numero» y así sucesivamente. Las
+ *       componentes de índices superiores al correspondiente al dígito más
  *       significativo almacenan el valor 0.
  */
 void convertir(const unsigned numero, unsigned naturalGrande[]);
 
 /*
- * Pre:  «naturalGrande» almacena los dígitos de un número natural lo suficientemente pequeño
- *       como para ser representado por un dato de tipo «unsigned». En la componente
- *       naturalGrande[0] se almacena el dígito que representa las unidades de ese número, en 
- *       la componente naturalGrande[1], las decenas y así sucesivamente. Las componentes de
- *       índices superiores al correspondiente al dígito más significativo almacenan el
- *       valor 0. «naturalGrande» tiene «NUM_DIGITOS» componentes.
+ * Pre:  «naturalGrande» almacena los dígitos de un número natural lo
+ *       suficientemente pequeño como para ser representado por un dato de tipo
+ *       «unsigned». En la componente naturalGrande[0] se almacena el dígito
+ *       que representa las unidades de ese número, en la componente
+ *       naturalGrande[1], las decenas y así sucesivamente. Las componentes de
+ *       índices superiores al correspondiente al dígito más significativo
+ *       almacenan el valor 0. «naturalGrande» tiene «NUM_DIGITOS»
+ *       componentes.
  * Post: Devuelve el valor numérico del natural almacenado en «naturalGrande».
  */
 unsigned valor(const unsigned naturalGrande[]);
 
 /*
  * Pre:  «a» y «b» almacenan las representaciones de sendos números naturales. 
- *       Las componentes a[0] y b[0] representan las unidades del natural «a» y «b»,
- *       respectivamente, las componentes a[1] y b[1], las decenas y así sucesivamente. Las
- *       componentes de índices superiores a los correspondientes a los dígitos más
- *       significativos almacenan el valor 0.
+ *       Las componentes a[0] y b[0] representan las unidades del natural «a» y
+ *       «b», respectivamente, las componentes a[1] y b[1], las decenas y así
+ *       sucesivamente. Las componentes de índices superiores a los
+ *       correspondientes a los dígitos más significativos almacenan el
+ *       valor 0.
  *       Los vectores «a», «b» y «suma» tienen «NUM_DIGITOS» componentes.
- * Post: Si la suma de los naturales representados por «a» y «b» tiene «NUM_DIGITOS» o menos,
- *       tras ejecutarse este procedimiento, «suma» almacena la representación del número
- *       natural equivalente a la suma de los naturales representados por «a» y «b» y el valor
- *       de «desbordamiento» es «false». En el caso contrario, el valor de «suma» no está
- *       definido y el valor de «desbordamiento» es «true». 
+ * Post: Si la suma de los naturales representados por «a» y «b» tiene
+ *       «NUM_DIGITOS» o menos, tras ejecutarse este procedimiento, «suma»
+ *       almacena la representación del número natural equivalente a la suma de
+ *       los naturales representados por «a» y «b» y el valor de
+ *       «desbordamiento» es «false». En el caso contrario, el valor de «suma»
+ *       no está definido y el valor de «desbordamiento» es «true». 
  */
-void sumar(const unsigned a[], const unsigned b[], unsigned suma[], bool &desbordamiento);
+void sumar(const unsigned a[], const unsigned b[],
+           unsigned suma[], bool &desbordamiento);
 
 /*
- * Pre:  «a» almacena la representación de un número natural. La componente a[0] representa las
- *       unidades del natural «a», la componente a[1], las decenas y así sucesivamente. Las
- *       componentes de índices superiores a los correspondientes a los dígitos más
- *       significativos almacenan el valor 0.
+ * Pre:  «a» almacena la representación de un número natural. La componente
+ *       a[0] representa las unidades del natural «a», la componente a[1], las
+ *       decenas y así sucesivamente. Las componentes de índices superiores a
+ *       los correspondientes a los dígitos más significativos almacenan el
+ *       valor 0.
  *       Los vectores «a» y «producto» tienen «NUM_DIGITOS» componentes.
- * Post: Si el producto de los naturales representados por «a» y el valor de «b» tiene
- *       «NUM_DIGITOS» o menos, tras ejecutarse este procedimiento, «producto» almacena la
- *       representación del número natural equivalente al producto de los naturales
- *       representados por «a» y el valor de «b» y el valor de «desbordamiento» es «false».
- *       En el caso contrario, el valor de «producto» no está definido y el valor de
- *       «desbordamiento» es «true».
+ * Post: Si el producto de los naturales representados por «a» y el valor de
+ *       «b» tiene «NUM_DIGITOS» o menos, tras ejecutarse este procedimiento,
+ *       «producto» almacena la representación del número natural equivalente
+ *       al producto de los naturales representados por «a» y el valor de «b» y
+ *       el valor de «desbordamiento» es «false». En el caso contrario, el
+ *       valor de «producto» no está definido y el valor de «desbordamiento»
+ *       es «true».
  */
-void multiplicar(const unsigned a[], const unsigned b, unsigned producto[], bool &desbordamiento);
+void multiplicar(const unsigned a[], const unsigned b,
+                 unsigned producto[], bool &desbordamiento);
 
 /*
- * Pre:  «natural» almacena la representación de un número natural. La componente natural[0]
- *       representa las unidades de «natural», la componente natural[1] las decenas de
- *       «natural» y así sucesivamente. Las componentes de índices superiores al
- *       correspondiente al dígito más significativo almacenan el valor 0. «natural» e
- *       «imagen» tienen «NUM_DIGITOS» componentes.
- * Post: Tras ejecutar este procedimiento, «imagen» almacena la representación del número
- *       natural correspondiente a la imagen especular de «natural».
+ * Pre:  «natural» almacena la representación de un número natural. La
+ *       componente natural[0] representa las unidades de «natural», la
+ *       componente natural[1] las decenas de «natural» y así sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
+ *       «natural» e «imagen» tienen «NUM_DIGITOS» componentes.
+ * Post: Tras ejecutar este procedimiento, «imagen» almacena la representación
+ *       del número natural correspondiente a la imagen especular de «natural».
  */
 void calcularImagen(const unsigned natural[], unsigned imagen[]);
 
 /*
- * Pre:  «natural» almacena la representación de un número natural. La componente natural[0]
- *       representa las unidades de «natural», la componente natural[1] las decenas de
- *       «natural» y así sucesivamente. Las componentes de índices superiores al
- *       correspondiente al dígito más significativo almacenan el valor 0.
+ * Pre:  «natural» almacena la representación de un número natural. La
+ *       componente natural[0] representa las unidades de «natural», la
+ *       componente natural[1] las decenas de «natural» y así sucesivamente.
+ *       Las componentes de índices superiores al correspondiente al dígito más
+ *       significativo almacenan el valor 0.
  *       «natural» tiene «NUM_DIGITOS» componentes.
  * Post: Devuelve «true» si y solo si «natural» es un número capicúa.
  */
